e5f896bb1363dc67a76bbc9fc2fbd020b7500927
Caio-CGodoy <2109745@aluno.univesp.br>
2023-04-23 17:21:28 -0300

    Update settings.py

--- hangar252/settings.py
+++ hangar252/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = 'django-insecure-#ac(u@dpqgni^hu!g9kv&b-5c3^s%&6oixeat^!_u26qpjbq#y
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app', '127.0.0.1']
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh', '127.0.0.1']
 
 
 # Application definition
